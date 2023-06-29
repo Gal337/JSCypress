fc6a6f02543abe2f87252f06561c5b17e22bd94f
Gal Sesek <gal.sesek@endava.com>
2023-06-29 16:02:45 +0200

    Modified Test1.js file

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -10,6 +10,6 @@ describe("My First Test Suite", function () {
     cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
     cy.get(".search-keyword").type("ca");
     cy.wait(2000);
-    cy.get(".product:visible").should('have.length',4)
+    cy.get(".product:visible").should('have.length',4);
   })
 });
