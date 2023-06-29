4ec234f2ca6b08b65efc8d82ccf24297bb67f3dc
Gal Sesek <gal.sesek@endava.com>
2023-06-29 12:03:13 +0200

    Added HowToCypress file

---
+++ HowToCypress.js
@@ -0,0 +1,16 @@
+/*INSTRUCTIONS ON INSTALLING CYPRESS*/
+/*
+1. Create new project
+2. Open terminal
+3. In terminal window type:
+npm install cypress --save-dev (--save-dev might be optional #notsure)
+ */
+
+/*INSTRUCTIONS ON RUNNING THE TEST RUNNER*/
+/*
+1. After Cypress is installed and node_modules folder created in project structure
+2. In terminal window type:
+node_modules/.bin/cypress open
+3. Click E2E Testing or Component testing (depends on what you want)
+4. Choose Browser (if you clicked E2E Testing)
+*/
