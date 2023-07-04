e5f1ce7b14e0d9ee053308be14d4bc4e694823a4
Gal Sesek <gal.sesek@endava.com>
2023-07-04 15:48:39 +0200

    Updated Test2 file

--- cypress/integration/examples/Test2.js
+++ cypress/integration/examples/Test2.js
@@ -1,7 +1,7 @@
 /// <reference types="Cypress" />
 
 describe("My Second Test Suite", function () {
-  it('My first test case', function () {
+  it('Opening webpage', function () {
     cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
     cy.get(".search-keyword").type("ca");
   });
@@ -12,11 +12,16 @@ describe("My Second Test Suite", function () {
     cy.wait(2000);
     cy.get(".products").as("productsLocator");
     cy.get("@productsLocator").find(".product").each(($el, index, $list) => {
+
       const productName = $el.find("h4.product-name").text();
       if(productName.includes("Cashews"))
       {
         cy.wrap($el).find("button").click();
       }
+      
     });
+    cy.get(".cart-icon > img").click();
+    cy.contains("PROCEED TO CHECKOUT").click();
+    cy.contains("Place Order").click();
   });
 });
